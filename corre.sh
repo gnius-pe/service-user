4a68e08d1e89fef3b52906cc89f6de423429423b
igor <igor.ramos.c@hotmail.com>
2024-02-06 18:07:38 -0500

    delete

--- corre.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Instalación de dependencias de npm
-
-# Instalación global de nodemon
-npm install -g nodemon
-
-# Ejecución del script npm run dev
-npm run dev
--- src/app.ts
+++
@@ -1,4 +0,0 @@
-import express from "express";
-
-const app = express();
-app.listen(4000,()=> console.log('Servicio en el puerto 4000'));
